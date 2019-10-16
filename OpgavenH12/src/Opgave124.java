d830724142c472b4a814b65c404ef960a2523dee
DESKTOP-GT6JL2R\gerja <gerjan.maas@gmail.com>
2019-10-16 18:20:22 +0200

    Commit from IntelliJ-2

--- OpgavenH12/src/Opgave124.java
+++ OpgavenH12/src/Opgave124.java
@@ -1,6 +1,15 @@
 public class Opgave124 {
     public static int indexEersteA(String s) {
         int lengteString = s.length();
-        return lengteString;
+        int eersteA = -1;
+        for (int i = 0; i < lengteString; i++) {
+            String letter = String.valueOf(s.charAt(i));
+            if (eersteA == -1 && (letter.equals("A") || letter.equals("a"))) {
+                eersteA = i;
+            }
+        }
+        return eersteA;
     }
 }
+
+
--- OpgavenTest/src/OpgavenTester.java
+++ OpgavenTest/src/OpgavenTester.java
@@ -1,5 +1,5 @@
 public class OpgavenTester {
     public static void main(String[] args) {
-        System.out.println(Opgave124.indexEersteA("Gerjan"));
+        System.out.println(Opgave124.indexEersteA("Gerjan Maas"));
     }
 }
